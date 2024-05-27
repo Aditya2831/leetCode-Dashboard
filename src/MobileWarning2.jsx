84a7a58a0e0b2859a1d72763ab9d1396799be0dc
Aditya Gupta <109282622+Aditya2831@users.noreply.github.com>
2024-05-27 19:57:10 +0530

    Minor update

--- src/MobileWarning2.jsx
+++ src/MobileWarning2.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MobileWarning = () => {
+const MobileWarning2 = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 text-white text-2xl z-50 xl:hidden font-chakraPetch font-bold">
       PLEASE OPEN IT ON A DESKTOP
