a125951416dbf3b8497b742d00aff3ef565e0b0e
Aditya Gupta <109282622+Aditya2831@users.noreply.github.com>
2024-05-27 19:50:28 +0530

    Updated MobileWarning.jsx

---
+++ MobileWarning.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobileWarning = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 text-white text-2xl z-50 xl:hidden font-chakraPetch font-bold">
+      PLEASE OPEN IT ON A DESKTOP
+    </div>
+  );
+};
+
+export default MobileWarning;
